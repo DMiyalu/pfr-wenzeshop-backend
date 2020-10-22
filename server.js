0ea3b46b050b791a65986976e5ab3de34a59161b
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-22 12:38:22 +0100

    reset server

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const configToUse = require('./config/keys')
 const express = require('express')
 const app = express()
-const blogAdmin = express()
 const cors = require('cors')
 const mongoose = require("mongoose")
 const bodyParser = require('body-parser')
 const product = require('./src/routes/product')
 const user = require('./src/routes/user')
 const PORT = process.env.port || 8080
-const jwt = require("jsonwebtoken")
 
-
-var corsOptions = {
-    origin: '192.0.0.1:19006',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
 
 mongoose.connect(
@@ -37,17 +30,14 @@ mongoose.connect(
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/product', product)
+app.use('/user', user)
 
 //Routes
 app.get('/', (request, response) => {
     response.send('Welcome')
 })
-
-
-app.use('/product', product)
-app.use('/user', user)
-
 
 app.listen(PORT, () => {
     console.log(`Server running on localhost:${PORT}`)
